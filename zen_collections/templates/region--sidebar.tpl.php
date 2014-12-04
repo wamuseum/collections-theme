f792f8598a822571f2c7e816eb4461b6a194ce5a
Danny Murphy <murphy@danny-murphy.com>
2014-12-04 13:21:02 +0800

    add mediaqueries to sidebar

---
+++ zen_collections/templates/region--sidebar.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Returns HTML for a sidebar region.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728118
+ */
+?>
+<?php if ($content): ?>
+  <section class="<?php print $classes; ?> nomq-0-0 bb-1-3 mb-1-4 pb-1-5">
+    <?php print $content; ?>
+  </section>
+<?php endif; ?>
