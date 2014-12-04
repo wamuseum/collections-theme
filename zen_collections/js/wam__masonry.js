5031bcf56cde4952a12b74ec7781994ab236de42
Danny Murphy <murphy@danny-murphy.com>
2014-12-04 12:36:33 +0800

    Clean up wam masonry and make it pass JSLint

--- zen_collections/js/wam__masonry.js
+++ zen_collections/js/wam__masonry.js
@@ -1,41 +1,28 @@
-/*global Drupal:false  */
-(function($) {
+/*global Drupal:false, jQuery:false */
+(function ($) {
+  'use strict';
+  Drupal.behaviors.wamMasonry = {
+    attach: function () {
 
-	Drupal.behaviors.wamMasonry = {
-		attach: function () {
+      var $container = $('.masonry');
 
-			var $container = $('.masonry');
+      // Make masonry compatible with view ajax loading...
+      if ($container.data('masonry')) {
+        $container.masonry('destroy');
+      }
 
-            // Make masonry compatible with view ajax loading...
-            if ($container.data('masonry')) {
-                $container.masonry( 'destroy' );
-            }
+      // initialize
+      $container.masonry({
+      //columnWidth: 200,
+        itemSelector: '.masonry__item'
+      });
 
-			// initialize
-			$container.masonry({
-				//columnWidth: 200,
-				itemSelector: '.masonry__item'
-			});
-
-			$container.imagesLoaded( function() {
-		      $container.masonry({
-				itemSelector: '.masonry__item'
-		      });
-		    });
-
-
-//			$('.item-list-q a').click(function () {
-//				$container = $('.quicktabs-tabpage:not(.quicktabs-hide) .masonry .view-content');
-//				$container.masonry( 'destroy' );
-//				$container.masonry({
-//					//columnWidth: 200,
-//					itemSelector: '.masonry__item'
-//				});
-//			});
-			
-
-			
-		}
-	};
-})(jQuery);
+      $container.imagesLoaded(function () {
+        $container.masonry({
+          itemSelector: '.masonry__item'
+        });
+      });
+    }
+  };
+}(jQuery));
 
